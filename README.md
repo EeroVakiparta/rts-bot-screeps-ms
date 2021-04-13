b60c8839b3fb0f9d82ac7792089bd736161e1eaf
EeroVakiparta <eero.vakiparta@student.samk.fi>
2021-04-13 22:14:31 +0300

    First "working" version of the bot with no name
    - builder, harvester, upgrader

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # rts-bot-screeps-ms
-JS bot for MMO RTS https://docs.screeps.com/api/
+
+## JS bot for MMO RTS https://docs.screeps.com/api/
+
+# bot v0.1
+
+Can gather building materials, upgrade room controller, has potential to build something
+
+---
+
+## Starting own server (Ubuntu20.04LTS)
+
+    1 sudo apt update
+    2 sudo apt install nodejs
+    3 sudo apt install npm
+    4 sudo apt install python2
+    5 apt install build-essential
+    6 sudo apt-get update
+    7 sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release
+    8 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    9 sudo apt-get install docker-ce docker-ce-cli containerd.io
+    10 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    11 sudo apt-get update
+    12 sudo apt-get install docker-ce docker-ce-cli containerd.io
+    13 docker run -d --name screeps-server -v $PWD:/screeps -p 21025:21025 quay.io/ags131/screeps-server
+    14 docker run -it --rm -v $PWD:/screeps quay.io/ags131/screeps-server init
+    15 docker start screeps-server
--- main.js
+++ main.js
@@ -1,9 +1,75 @@
-var roleHarvester = require('role.harvester');
+var roleHarvester = require("role.harvester");
+var roleUpgrader = require("role.upgrader");
+var roleBuilder = require("role.builder");
 
 module.exports.loop = function () {
-
-    for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
-        roleHarvester.run(creep);
+  // Soul reaper
+  // -- For making sure there is not memory overload caused by souls of the dead creeps
+  for (var name in Memory.creeps) {
+    if (!Game.creeps[name]) {
+      delete Memory.creeps[name];
+      console.log("Clearing non-existing creep memory:", name);
     }
-}
+  }
+
+  // Harvester spawner
+  // --Make sure there is at least 2 harvesters at all times
+  var harvesters = _.filter(
+    Game.creeps,
+    (creep) => creep.memory.role == "harvester"
+  );
+  console.log("Harvesters: " + harvesters.length);
+
+  if (harvesters.length < 2) {
+    var newName = "Harvester" + Game.time;
+    console.log("Spawning new harvester: " + newName);
+    Game.spawns["Spawn1"].spawnCreep([WORK, CARRY, MOVE], newName, {
+      memory: { role: "harvester" },
+    });
+  }
+
+  // Builder spawner
+  // --Make sure there is at least 1 builder at all times
+  var harvesters = _.filter(
+    Game.creeps,
+    (creep) => creep.memory.role == "builder"
+  );
+  console.log("Builders: " + harvesters.length);
+
+  if (harvesters.length < 1) {
+    var newName = "Builder" + Game.time;
+    console.log("Spawning new builder: " + newName);
+    Game.spawns["Spawn1"].spawnCreep([WORK, CARRY, MOVE], newName, {
+      memory: { role: "builder" },
+    });
+  }
+
+  // Upgrader spawner
+  // --Make sure there is at least 1 upgrader at all times
+  var harvesters = _.filter(
+    Game.creeps,
+    (creep) => creep.memory.role == "upgrader"
+  );
+  console.log("Upgrader: " + harvesters.length);
+
+  if (harvesters.length < 1) {
+    var newName = "Upgrader" + Game.time;
+    console.log("Spawning new upgrader: " + newName);
+    Game.spawns["Spawn1"].spawnCreep([WORK, CARRY, MOVE], newName, {
+      memory: { role: "upgrader" },
+    });
+  }
+
+  for (var name in Game.creeps) {
+    if (Game.creeps) var creep = Game.creeps[name];
+    if (creep.memory.role == "harvester") {
+      roleHarvester.run(creep);
+    }
+    if (creep.memory.role == "upgrader") {
+      roleUpgrader.run(creep);
+    }
+    if (creep.memory.role == "builder") {
+      roleBuilder.run(creep);
+    }
+  }
+};
