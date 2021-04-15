cf9091395ec746b36bed477eec0507db7be43d3c
Eero Vakiparta <eero.vakiparta@student.samk.fi>
2021-04-15 18:47:34 +0300

    Introduced filling priorities for harvester

--- role.harvester.js
+++ role.harvester.js
@@ -8,22 +8,38 @@ var roleHarvester = {
               creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
           }
       }
-      else {
-          var targets = creep.room.find(FIND_STRUCTURES, {
+      else{
+          var priorityTargets = creep.room.find(FIND_STRUCTURES, {
               filter: (structure) => {
                   return (structure.structureType == STRUCTURE_EXTENSION ||
-                      structure.structureType == STRUCTURE_SPAWN ||
+                      structure.structureType == STRUCTURE_SPAWN) &&
+                      structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+              }
+          });
+          if(priorityTargets.length > 0) {
+              if(creep.transfer(priorityTargets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                  creep.moveTo(priorityTargets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+              }
+          }
+          else {
+            var secondaryTargets = creep.room.find(FIND_STRUCTURES, {
+              filter: (structure) => {
+                  return (structure.structureType == STRUCTURE_CONTAINER ||
                       structure.structureType == STRUCTURE_TOWER) &&
                       structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
               }
           });
-          if(targets.length > 0) {
-              if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                  creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+          //TODO: remove duplicates & speed up
+          if(secondaryTargets.length > 0) {
+              if(creep.transfer(secondaryTargets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                  creep.moveTo(secondaryTargets[0], {visualizePathStyle: {stroke: '#ffffff'}});
               }
           }
           else {
-              creep.moveTo(33,16); // remove full harvesters blockin the mine
+            // TODO: make better way to have harvester move away when it has nothing to do
+              creep.say("😪");
+              creep.moveTo(33,16); 
+          }
           }
       }
   }
