6240074e79bc280691f97c9dfdd573e163268248
EeroVakiparta <eero.vakiparta@student.samk.fi>
2021-04-17 15:30:24 +0300

    Prevented healer not bouncing between targets

--- role.healer.js
+++ role.healer.js
@@ -1,16 +1,17 @@
 var roleHealer = {
 	/** @param {Creep} creep **/
 	run: function (creep) {
-	    // TODO: make healer avoid enemies, maybe choose ranged heal instead?
+		// TODO: make healer avoid enemies, maybe choose ranged heal instead?
 		const targets = creep.room.find(FIND_MY_CREEPS, {
 			filter: (object) => object.hits < object.hitsMax,
 		});
 
-		targets.sort((a, b) => a.hits - b.hits);
-
 		if (targets.length > 0) {
+			targets.sort((a, b) => a.hits - b.hits);
+
 			if (creep.heal(targets[0]) == ERR_NOT_IN_RANGE) {
-				creep.moveTo(targets[0]);
+				const closestLowHP = creep.pos.findClosestByRange(targets);
+				creep.moveTo(closestLowHP);
 			}
 		}
 	},
